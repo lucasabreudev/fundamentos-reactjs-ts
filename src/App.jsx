6a61aa25cd3c1cf853e5e485021bdabd5cfae26a
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-13 16:22:31 -0300

    propriedades de um componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ export function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Teste"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, omnis deleniti veritatis ullam vitae soluta natus eaque voluptates. Sit cum voluptatibus architecto possimus voluptate at? Nam quisquam consequatur impedit consectetur?"
+      />
+
     </div>
   )
 }
