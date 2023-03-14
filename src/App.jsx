e3034560bae70a90ced8198dfd0ddf72225e4892
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-14 14:24:43 -0300

    feat: add global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Post } from "./Post";
+import { Header } from './components/Header';
+
+import './global.css';
 
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post
         author="Teste"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, omnis deleniti veritatis ullam vitae soluta natus eaque voluptates. Sit cum voluptatibus architecto possimus voluptate at? Nam quisquam consequatur impedit consectetur?"
       />
-
     </div>
   )
 }
