e6f1814d0dba4fe10ec3236fd407c8d4b695b294
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-14 19:50:45 -0300

    feat: criando componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { Post } from "./Post";
 import { Header } from './components/Header';
+import { Sidebar } from "./components/Sidebar";
 
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post
-        author="Teste"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, omnis deleniti veritatis ullam vitae soluta natus eaque voluptates. Sit cum voluptatibus architecto possimus voluptate at? Nam quisquam consequatur impedit consectetur?"
-      />
+
+      <div className={styles.wrapper} >
+        <Sidebar />
+        <main>
+          <Post
+            author="Teste"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, omnis deleniti veritatis ullam vitae soluta natus eaque voluptates. Sit cum voluptatibus architecto possimus voluptate at? Nam quisquam consequatur impedit consectetur?"
+          />
+          <Post
+            author="Teste"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, omnis deleniti veritatis ullam vitae soluta natus eaque voluptates. Sit cum voluptatibus architecto possimus voluptate at? Nam quisquam consequatur impedit consectetur?"
+          />
+        </main>
+
+      </div>
+
     </div>
   )
 }
