7ebb14bb9b6d8910e9f722353c10c107e42d4ae3
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-16 13:41:46 -0300

    feat: criação do componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -12,7 +13,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/99924122?v=4" alt="" />
+                <Avatar src="https://avatars.githubusercontent.com/u/99924122?v=4" />
                 <strong>Lucas</strong>
                 <span>Front-end Developer</span>
             </div>
