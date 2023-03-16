b0e3e9fb2bfa08b7010ad23970066d1fb73a4721
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-16 06:53:15 -0300

    feat: criado componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/99924122?v=4" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Lucas</strong>
+                            <time title="15 de Março às 17h" dateTime="2023-03-15 17:00:10">Cerca de 1h atrás</time>
+
+                            <button title='Deletar comentário'>
+                                <Trash size={20} />
+                            </button>
+                        </div>
+                    </header>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem earum in nobis animi facilis, expedita obcaecati fugiat. Vel modi velit suscipit est quaerat saepe magnam aliquam amet iste obcaecati.
+                    </p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Curtir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
