e73f4d2d3d573ec39066f38e5fdbf0a5714a31c6
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-16 18:11:46 -0300

    feat: começando funcionalidade de listar posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,36 @@ import { Post } from './components/Post';
 import './global.css';
 import styles from './App.module.css';
 
-export function App() {
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/lucaslabreu.png',
+      name: 'Lucas Abreu',
+      role: 'Front-end Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Olá pessoal, boa noite!' },
+      { type: 'link', content: 'study' }
+    ],
+    publishedAt: new Date('2023-03-16 15:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/lucaslabreu.png',
+      name: 'Lucas Abreu',
+      role: 'Front-end Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Good evening!' },
+      { type: 'link', content: 'learning_english' }
+    ],
+    publishedAt: new Date('2022-03-16 17:00:00'),
+  }
+]
 
+export function App() {
   return (
     <div>
       <Header />
@@ -14,8 +42,15 @@ export function App() {
       <div className={styles.wrapper} >
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
